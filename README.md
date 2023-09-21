e3323b75f569198ee8c3f0a58bc607b4c5febce3
Happpydust <117503464+Happpydev@users.noreply.github.com>
2023-09-21 15:39:29 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Scratch-User-Profile-Time
+ A scratchatach program to put the time in your What I am Working On
+
+Inspried by <a herf="https://scratch.mit.edu/users/codeGIO/">codeGIO</a> profile
+
+Sorry, I'm too tired to write the rest of readme right now, give me a hour and I will write it.
---
+++ main.py
@@ -0,0 +1,47 @@
+import scratchattach as scratch3
+import requests
+import datetime
+session = scratch3.login("username", "password") 
+user = session.connect_user("username ") # Yes, you have to put your username in twice. Case Senstive
+
+def format_time(raw_time):
+    datetime_obj = datetime.datetime.fromisoformat(raw_time)
+    
+    formatted_time = datetime_obj.strftime("%A, %d %B %Y, %H:%M:%S")
+    return formatted_time
+
+def othersleep(seconds): # time.sleep() was broken when I ran it so this it what I am sticking with
+    start_time = datetime.datetime.now()
+    end_time = start_time + datetime.timedelta(seconds=seconds)
+    
+    while datetime.datetime.now() < end_time:
+        pass
+
+def get_current_time():
+    url = "http://worldtimeapi.org/api/ip"
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        data = response.json()
+        current_time = data['datetime']
+        return current_time
+    else:
+        return "Failed to retrieve time"
+
+
+while 1==1:
+    time = get_current_time()
+    current_time = get_current_time()
+    formatted_time = format_time(current_time)
+    formatted_time = format_time(time)
+    print("Current time:", formatted_time)
+    print('Sending to servers')
+    # Modify what's below to change what shows up on your wiwo 
+    # Add a line below f""" and change that to make it show up there!
+    wiwo_text  = f"""
+{formatted_time} (localized to my local pos)
+    """
+    user.set_wiwo(wiwo_text)
+    # Swap set_wiwo for set_bio to make it come on your About me
+    print('Done')
+    othersleep(30)
